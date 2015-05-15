2c6c644c5c53e1165ed03736aab184bce17798a1
Harrison Freni <harrison@harrisonfreni.me>
2015-05-15 09:23:33 -0700

    Added Optimal Pathfinder Code

---
+++ src/optimalPath.py
@@ -0,0 +1,16 @@
+import math
+
+
+# Robot Parameters Open
+METRES_PER_INCH = 0.0254
+FIELD_SIZE = 140.5
+GOAL_X = 4.0941
+GOAL_Y = 139.9059
+
+LAUNCH_ANGLE = input('Enter the Launch Angle in Degrees: ')
+LAUNCH_HEIGHT = input('Enter your Launch Height in Inches: ')
+INCREMENT = input('Enter a Distance Increment in Inches: ')
+# Robot Parameters Close
+
+#TODO: Load in Ball Location Parameters
+ballPos = array('d', [18, 36, 92])
